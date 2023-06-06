d316b4563c6d66367fd88b1df967997d01039f88
Jakub Levý <jakub.levy15@gmail.com>
2023-06-06 14:47:47 +0200

    nove formatovani

--- persons/apps.py
+++ persons/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PersonsConfig(AppConfig):
-    name = 'persons'
+    name = "persons"
--- persons/migrations/0001_initial.py
+++ persons/migrations/0001_initial.py
@@ -5,64 +5,143 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
-        ('events', '0001_initial'),
+        ("events", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Feature',
+            name="Feature",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('feature_type', models.CharField(choices=[('K', 'kvalifikace'), ('V', 'vlastnictví'), ('O', 'oprávnění')], max_length=1)),
-                ('category', models.CharField(max_length=20)),
-                ('name', models.CharField(max_length=50, unique=True)),
-                ('never_expires', models.BooleanField(default=False)),
-                ('tier', models.PositiveSmallIntegerField(default=0)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "feature_type",
+                    models.CharField(
+                        choices=[
+                            ("K", "kvalifikace"),
+                            ("V", "vlastnictví"),
+                            ("O", "oprávnění"),
+                        ],
+                        max_length=1,
+                    ),
+                ),
+                ("category", models.CharField(max_length=20)),
+                ("name", models.CharField(max_length=50, unique=True)),
+                ("never_expires", models.BooleanField(default=False)),
+                ("tier", models.PositiveSmallIntegerField(default=0)),
             ],
         ),
         migrations.CreateModel(
-            name='FeatureAssignment',
+            name="FeatureAssignment",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('date_assigned', models.DateField()),
-                ('date_expire', models.DateField()),
-                ('feature', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='persons.feature')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("date_assigned", models.DateField()),
+                ("date_expire", models.DateField()),
+                (
+                    "feature",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="persons.feature",
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Person',
+            name="Person",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('email', models.EmailField(max_length=254, unique=True)),
-                ('name', models.CharField(max_length=50)),
-                ('date_of_birth', models.DateField()),
-                ('person_type', models.CharField(choices=[('dite', 'člen dítě'), ('dospely', 'člen dospělý'), ('cekatel', 'člen čekatel'), ('cestny', 'čestný člen'), ('externi', 'externí spolupracovník'), ('rodic', 'rodič')], max_length=10)),
-                ('features', models.ManyToManyField(through='persons.FeatureAssignment', to='persons.feature')),
-                ('managed_people', models.ManyToManyField(to='persons.person')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("email", models.EmailField(max_length=254, unique=True)),
+                ("name", models.CharField(max_length=50)),
+                ("date_of_birth", models.DateField()),
+                (
+                    "person_type",
+                    models.CharField(
+                        choices=[
+                            ("dite", "člen dítě"),
+                            ("dospely", "člen dospělý"),
+                            ("cekatel", "člen čekatel"),
+                            ("cestny", "čestný člen"),
+                            ("externi", "externí spolupracovník"),
+                            ("rodic", "rodič"),
+                        ],
+                        max_length=10,
+                    ),
+                ),
+                (
+                    "features",
+                    models.ManyToManyField(
+                        through="persons.FeatureAssignment", to="persons.feature"
+                    ),
+                ),
+                ("managed_people", models.ManyToManyField(to="persons.person")),
             ],
         ),
         migrations.CreateModel(
-            name='Transaction',
+            name="Transaction",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('amount', models.IntegerField()),
-                ('reason', models.CharField(max_length=150)),
-                ('date', models.DateField()),
-                ('event', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='events.event')),
-                ('person', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='persons.person')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("amount", models.IntegerField()),
+                ("reason", models.CharField(max_length=150)),
+                ("date", models.DateField()),
+                (
+                    "event",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        to="events.event",
+                    ),
+                ),
+                (
+                    "person",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="persons.person"
+                    ),
+                ),
             ],
         ),
         migrations.AddField(
-            model_name='featureassignment',
-            name='person',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='persons.person'),
+            model_name="featureassignment",
+            name="person",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to="persons.person"
+            ),
         ),
         migrations.AlterUniqueTogether(
-            name='featureassignment',
-            unique_together={('person', 'feature')},
+            name="featureassignment",
+            unique_together={("person", "feature")},
         ),
     ]
