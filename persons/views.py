1f45df8c9eae3698b41f8a6f29e7776ff3bc1f54
Vojtech Svandelik <vojtech.svandelik@gmail.com>
2023-06-08 13:37:59 +0200

    zakladni pridavani a uprava osob, zmena jazyka systemu

--- persons/views.py
+++ persons/views.py
@@ -1,33 +1,34 @@
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import PersonForm
 from .models import Person
 
 
 class IndexView(generic.ListView):
     model = Person
-    template_name = 'persons/index.html'
-    context_object_name = 'persons'
+    template_name = "persons/index.html"
+    context_object_name = "persons"
 
 
 class DetailView(generic.DetailView):
     model = Person
-    template_name = 'persons/detail.html'
+    template_name = "persons/detail.html"
 
 
 class PersonCreateView(generic.edit.CreateView):
     model = Person
-    template_name = 'persons/edit.html'
-    fields = ['email', 'first_name', 'last_name', 'date_of_birth', 'person_type']
+    template_name = "persons/edit.html"
+    form_class = PersonForm
 
 
 class PersonUpdateView(generic.edit.UpdateView):
     model = Person
-    template_name = 'persons/edit.html'
-    fields = ['email', 'first_name', 'last_name', 'date_of_birth', 'person_type']
+    template_name = "persons/edit.html"
+    form_class = PersonForm
 
 
 class PersonDeleteView(generic.edit.DeleteView):
     model = Person
-    template_name = 'persons/confirm_delete.html'
+    template_name = "persons/confirm_delete.html"
     success_url = reverse_lazy("persons:index")
--- vzs/settings.py
+++ vzs/settings.py
@@ -57,15 +57,15 @@ ROOT_URLCONF = "vzs.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -84,7 +84,6 @@ DATABASES = {
     }
 }
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
@@ -110,14 +109,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "cs"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Prague"
 
 USE_I18N = True
 
 USE_TZ = True
 
+DATE_INPUT_FORMATS = "%Y-%m-%d"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
