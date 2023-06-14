21ce53d55dea2a63d331c34a8b4a3c490861e9d9
Peter Fačko <pfacko1@gmail.com>
2023-06-14 17:29:56 +0200

    validacia hesla

--- vzs/settings.py
+++ vzs/settings.py
@@ -96,12 +96,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 8,
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+    {
+        "NAME": "users.passwords.MinimumNumericValidator",
+    },
+    {
+        "NAME": "users.passwords.MinimumCapitalValidator",
     },
 ]
 
