5be48d91f3a700cbfc2a9358625d6a63245e493b
Peter Fačko <pfacko1@gmail.com>
2023-06-28 20:09:11 +0200

    rc 1

--- users/urls.py
+++ users/urls.py
@@ -6,11 +6,6 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("pridat/", views.UserCreateView.as_view(), name="add"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/smazat/", views.UserDeleteView.as_view(), name="delete"),
-    path("<int:pk>/upravit/", views.UserEditView.as_view(), name="edit"),
     path("prihlasit/", views.LoginView.as_view(), name="login"),
     path("odhlasit/", auth.views.LogoutView.as_view(), name="logout"),
     path("povoleni/", views.PermissionsView.as_view(), name="permissions"),
--- users/views.py
+++ users/views.py
@@ -12,19 +12,33 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from persons.models import Person
 from .models import User, Permission
 from . import forms
-from persons.models import Person
 
 
 class UserCreateView(SuccessMessageMixin, generic.edit.CreateView):
     template_name = "users/create.html"
     form_class = forms.UserCreateForm
-    success_url = reverse_lazy("users:add")
+    queryset = Person.objects.filter(user__isnull=True)
+
+    def get_success_url(self):
+        return reverse("persons:detail", kwargs={"pk": self.person.pk})
 
     def get_success_message(self, cleaned_data):
         return _(f"{self.object} byl úspěšně přidán.")
 
     def get_initial(self):
         return {"person": self.request.GET.get("person")}
+
+    def dispatch(self, request, *args, **kwargs):
+        self.person = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+
+        if "person" not in kwargs:
+            kwargs["person"] = self.person
+
+        return kwargs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -37,6 +51,9 @@ class UserCreateView(SuccessMessageMixin, generic.edit.CreateView):
 
         if "person_select_form" not in context:
             context["person_select_form"] = forms.PersonSelectForm(self.request.GET)
+
+        if "person" not in context:
+            context["person"] = self.person
 
         return context
 
@@ -55,25 +72,29 @@ class DetailView(generic.detail.DetailView):
 class UserDeleteView(SuccessMessageMixin, generic.edit.DeleteView):
     model = User
     template_name = "users/delete.html"
-    success_url = reverse_lazy("users:index")
+
+    def get_success_url(self):
+        return reverse("persons:detail", kwargs={"pk": self.person.pk})
 
     def form_valid(self, form):
-        # success_message is sent after object deletion so we need to save the string
+        # success_message is sent after object deletion so we need to save the data
+        # we will need later
         self.user_representation = str(self.object)
+        self.person = self.object.person
         return super().form_valid(form)
 
     def get_success_message(self, cleaned_data):
         return _(f"{self.user_representation} byl úspěšně odstraněn.")
 
 
-class UserEditView(SuccessMessageMixin, generic.edit.UpdateView):
+class UserChangePasswordView(SuccessMessageMixin, generic.edit.UpdateView):
     model = User
-    template_name = "users/edit.html"
-    form_class = forms.UserEditForm
-    success_message = _("Uživatel byl úspěšně upraven.")
+    template_name = "users/change_password.html"
+    form_class = forms.UserChangePasswordForm
+    success_message = _("Heslo bylo úspěšně změněno.")
 
     def get_success_url(self):
-        return reverse_lazy("users:detail", kwargs={"pk": self.object.pk})
+        return reverse("persons:detail", kwargs={"pk": self.object.pk})
 
 
 def set_active_person(request, person):
@@ -84,6 +105,9 @@ class LoginView(auth_views.LoginView):
     template_name = "users/login.html"
     authentication_form = forms.LoginForm
     redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse("persons:detail", kwargs={"pk": self.request.user.person.pk})
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -132,24 +156,51 @@ class PermissionDetailView(generic.detail.DetailView):
     template_name = "users/permission_detail.html"
 
 
-class PermissionAssignView(
-    generic.detail.SingleObjectTemplateResponseMixin,
-    generic.detail.SingleObjectMixin,
-    generic.edit.BaseFormView,
+class UserAssignRemovePermissionView(
+    generic.detail.SingleObjectMixin, generic.edit.FormView
 ):
-    http_method_names = ["post"]
-    form_class = forms.PermissionAssignForm
-    model = User
+    form_class = forms.UserAssignRemovePermissionForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=User.objects.all())
+        return super().dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
-        return reverse_lazy("users:detail", kwargs={"pk": self.object.pk})
+        return reverse("persons:detail", kwargs={"pk": self.object.person.pk})
+
+    def _change_user_permission(self, user, permission):
+        pass
 
     def form_valid(self, form):
-        self.object = self.get_object()
-
         user = self.object
         permission = form.cleaned_data["permission"]
 
-        user.user_permissions.add(permission)
+        self._change_user_permission(user, permission)
 
         return super().form_valid(form)
+
+
+class UserAssignPermissionView(
+    generic.list.MultipleObjectMixin, UserAssignRemovePermissionView
+):
+    template_name = "users/assign_permission.html"
+
+    def get_context_data(self, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        # TODO should be:
+        # return Permission.objects.difference(self.object.user_permissions.all())
+        # but SQlite doesn't support it, change when we start using PostgreSQL
+        return Permission.objects.all()
+
+    def _change_user_permission(self, user, permission):
+        user.user_permissions.add(permission)
+
+
+class UserRemovePermissionView(UserAssignRemovePermissionView):
+    http_method_names = ["post"]
+
+    def _change_user_permission(self, user, permission):
+        user.user_permissions.remove(permission)
