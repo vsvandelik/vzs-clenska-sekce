a3daff6ed00ec95f46dac4fae138581556ec6a31
Vojtech Svandelik <vojtech.svandelik@gmail.com>
2023-07-10 13:42:06 +0200

    odebrani nejakych tinymce moznosti

--- vzs/settings.py
+++ vzs/settings.py
@@ -153,13 +153,13 @@ STATICFILES_DIRS = [
 
 TINYMCE_DEFAULT_CONFIG = {
     "menubar": "file edit view insert format tools table help",
-    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
-    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
-    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "plugins": "advlist autolink lists link image charmap preview anchor searchreplace  code "
+    "fullscreen  media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontsizeselect formatselect | alignleft "
     "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
     "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
-    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
-    "a11ycheck ltr rtl | showcomments addcomment code",
+    "insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl",
     "custom_undo_redo_levels": 10,
 }
 
