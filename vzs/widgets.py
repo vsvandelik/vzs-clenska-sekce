64f064e46c02be70d5baef5d4cdba70d1e14737b
Vojtech Svandelik <vojtech.svandelik@gmail.com>
2023-07-11 18:11:58 +0200

    refaktorovan widgets

--- vzs/widgets.py
+++ vzs/widgets.py
@@ -3,38 +3,26 @@ from tempus_dominus import widgets
 
 class DatePickerWithIcon(widgets.DatePicker):
     def __init__(self, attrs=None, options=None, format=None):
-        if not attrs:
-            attrs = {}
-
-        if "append" not in attrs:
-            attrs["append"] = "fas fa-calendar"
+        attrs = attrs or {}
+        attrs.setdefault("append", "fas fa-calendar")
 
         super().__init__(attrs, options, format)
 
 
 class DateTimePickerWithIcon(widgets.DateTimePicker):
     def __init__(self, attrs=None, options=None, format=None):
-        if not attrs:
-            attrs = {}
+        attrs = attrs or {}
+        attrs.setdefault("append", "fas fa-calendar")
 
-        if "append" not in attrs:
-            attrs["append"] = "fas fa-calendar"
-
-        if not options:
-            options = {}
-
-        if "icons" not in options:
-            options["icons"] = {"time": "far fa-clock"}
+        options = options or {}
+        options.setdefault("icons", {"time": "far fa-clock"})
 
         super().__init__(attrs, options, format)
 
 
 class TimePickerWithIcon(widgets.TimePicker):
     def __init__(self, attrs=None, options=None, format=None):
-        if not attrs:
-            attrs = {}
-
-        if "append" not in attrs:
-            attrs["append"] = "far fa-clock"
+        attrs = attrs or {}
+        attrs.setdefault("append", "far fa-clock")
 
         super().__init__(attrs, options, format)
