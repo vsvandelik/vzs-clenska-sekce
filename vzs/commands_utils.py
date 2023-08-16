24fedf80184b1d7b33f33157544fb85bd0365a01
Jakub Levý <jakub.levy15@gmail.com>
2023-08-16 10:29:16 +0200

    lower_bound, upper_bound podminky je obracene

--- vzs/commands_utils.py
+++ vzs/commands_utils.py
@@ -1,23 +1,23 @@
 from argparse import ArgumentTypeError
 
 
-def lower_bounded_int(value, lower_bound_exclusive):
+def lower_bounded_int(value, lower_bound_inclusive):
     v = int(value)
-    if v < lower_bound_exclusive:
+    if v < lower_bound_inclusive:
         raise ArgumentTypeError(f"{v} is an invalid value")
     return v
 
 
-def upper_bounded_int(value, upper_bound_exclusive):
+def upper_bounded_int(value, upper_bound_inclusive):
     v = int(value)
-    if v > upper_bound_exclusive:
+    if v > upper_bound_inclusive:
         raise ArgumentTypeError(f"{v} is an invalid value")
     return v
 
 
 def age_int(value):
-    lower_bounded_int(value, 0)
-    return upper_bounded_int(value, 100)
+    lower_bounded_int(value, 1)
+    return upper_bounded_int(value, 99)
 
 
 def positive_int(value):
