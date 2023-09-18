b04becb6f92b98a9578c316d884f5f399de6b512
Peter Fačko <pfacko1@gmail.com>
2023-09-18 14:35:30 +0200

    PersonSerializer managed_persons fix

--- persons/serializers.py
+++ persons/serializers.py
@@ -1,9 +1,21 @@
-from persons.models import Person
+from .models import Person
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    HyperlinkedModelSerializer,
+    HyperlinkedRelatedField,
+)
 
 
-class PersonSerializer(ModelSerializer):
+class PersonSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Person
         fields = "__all__"
+        extra_kwargs = {
+            "url": {"view_name": "api:person-detail"},
+        }
+
+    managed_persons = HyperlinkedRelatedField(
+        queryset=Person.objects.all(),
+        many=True,
+        view_name="api:person-detail",
+    )
