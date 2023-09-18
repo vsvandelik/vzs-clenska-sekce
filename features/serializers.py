f475313cfad017b7376266e81b6a262fb313c1e2
Peter Fačko <pfacko1@gmail.com>
2023-09-18 14:47:09 +0200

    FeatureSerializer hyperlinked

--- features/serializers.py
+++ features/serializers.py
@@ -1,9 +1,13 @@
 from .models import Feature
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer
 
 
-class FeatureSerializer(ModelSerializer):
+class FeatureSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Feature
         exclude = ["lft", "rght", "tree_id", "level"]
+        extra_kwargs = {
+            "url": {"view_name": "api:feature-detail"},
+            "parent": {"view_name": "api:feature-detail"},
+        }
