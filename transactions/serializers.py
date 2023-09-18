a37ca1e70dd26c5c3e6394b3a50d8c14f70a66ea
Peter Fačko <pfacko1@gmail.com>
2023-09-18 15:16:15 +0200

    TransactionSerializer fix

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,9 +1,20 @@
-from transactions.models import Transaction
+from .models import Transaction
 
-from rest_framework.serializers import ModelSerializer
+from events.models import Event
+
+from rest_framework.serializers import (
+    HyperlinkedModelSerializer,
+    PrimaryKeyRelatedField,
+)
 
 
-class TransactionSerializer(ModelSerializer):
+class TransactionSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Transaction
         fields = "__all__"
+        extra_kwargs = {
+            "url": {"view_name": "api:transaction-detail"},
+            "person": {"view_name": "api:person-detail"},
+        }
+
+    event = PrimaryKeyRelatedField(queryset=Event.objects.all())
