5fe77fcf19ca87a5aa0464003079f57c3ad57505
Peter Fačko <pfacko1@gmail.com>
2023-11-14 18:54:04 +0100

    `users.forms`

--- users/models.py
+++ users/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import models as auth_models
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models import Q
-from django.urls import reverse
 from django.utils import timezone
 from django.utils.functional import classproperty
 from django.utils.translation import gettext_lazy as _
