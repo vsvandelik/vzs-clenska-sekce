3c0b6a088ee590c8a1c5eb470ad7ba21806c1ae9
Peter Fačko <pfacko1@gmail.com>
2024-01-07 18:25:32 +0100

    anotacie groups.views

--- groups/views.py
+++ groups/views.py
@@ -26,12 +26,34 @@ from .permissions import GroupPermissionMixin
 
 
 class GroupIndexView(GroupPermissionMixin, ListView):
+    """
+    Displays the list of all groups.
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+    """
+
     context_object_name = "groups"
     model = Group
     template_name = "groups/index.html"
 
 
 class GroupDeleteView(GroupPermissionMixin, SuccessMessageMixin, DeleteView):
+    """
+    Deletes a group.
+
+    **Success redirection view**: :class:`groups.views.GroupIndexView`
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+
+    **Path parameters**:
+
+    *   ``pk`` - group ID
+    """
+
     model = Group
     success_message = "Skupina byla úspěšně smazána."
     success_url = reverse_lazy("groups:index")
@@ -39,10 +61,28 @@ class GroupDeleteView(GroupPermissionMixin, SuccessMessageMixin, DeleteView):
 
 
 class GroupDetailView(GroupPermissionMixin, DetailView):
+    """
+    Displays the detail of a group.
+
+    Allows selecting persons to be added to the group.
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+
+    **Path parameters**:
+
+    *   ``pk`` - group ID
+    """
+
     model = Group
     template_name = "groups/detail.html"
 
     def get_context_data(self, **kwargs):
+        """
+        *   ``available_persons`` - persons who can be added to the group
+        """
+
         kwargs.setdefault(
             "available_persons",
             Person.objects.exclude(
@@ -53,15 +93,40 @@ class GroupDetailView(GroupPermissionMixin, DetailView):
         return super().get_context_data(**kwargs)
 
 
-class GroupAddMembersView(GroupPermissionMixin, SuccessMessageMixin, UpdateView):
+class GroupAddMembersView(GroupPermissionMixin, MessagesMixin, UpdateView):
+    """
+    Adds persons to a group.
+
+    Also adds the persons to the Google group if the group has one specified.
+
+    **Success redirection view**: :class:`groups.views.GroupDetailView`
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+
+    **Path parameters**:
+
+    *   ``pk`` - group ID
+
+    **Request body parameters**:
+
+    *   ``members`` - list of persons to be added to the group
+    """
+
+    error_message = _("Nepodařilo se přidat osoby.")
     form_class = AddMembersGroupForm
     model = Group
-    success_message = "Osoby byly úspěšně přidány."
+    success_message = _("Osoby byly úspěšně přidány.")
 
     def get_success_url(self):
+        """:meta private:"""
+
         return reverse("groups:detail", args=(self.object.pk,))
 
     def form_valid(self, form):
+        """:meta private:"""
+
         new_members = form.cleaned_data["members"]
 
         existing_members = self.object.members.all()
@@ -79,44 +144,92 @@ class GroupAddMembersView(GroupPermissionMixin, SuccessMessageMixin, UpdateView)
 
         return super().form_valid(form)
 
-    def form_invalid(self, form):
-        error_message(self.request, _("Nepodařilo se přidat osoby."))
-        return super().form_invalid(form)
 
+class GroupCreateEditMixin(GroupPermissionMixin, MessagesMixin):
+    """:meta private:"""
 
-class GroupCreateEditMixin(GroupPermissionMixin, SuccessMessageMixin):
+    error_message = _("Skupinu se nepodařilo uložit.")
     form_class = GroupForm
     model = Group
-    success_message = "Skupina byla úspěšně uložena."
+    success_message = _("Skupina byla úspěšně uložena.")
     template_name = "groups/edit.html"
 
     def get_success_url(self):
         return reverse(f"groups:detail", args=(self.object.pk,))
 
-    def form_invalid(self, form):
-        error_message(self.request, _("Skupinu se nepodařilo uložit."))
-        return super().form_invalid(form)
-
 
 class GroupCreateView(GroupCreateEditMixin, CreateView):
+    """
+    Creates a new group.
+
+    **Success redirection view**: :class:`groups.views.GroupDetailView`
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+
+    **Request body parameters**:
+
+    *   ``name``
+    *   ``google_email``
+    *   ``google_as_members_authority``
+    """
+
     pass
 
 
 class GroupEditView(GroupCreateEditMixin, UpdateView):
+    """
+    Edits a group.
+
+    **Success redirection view**: :class:`groups.views.GroupDetailView`
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+
+    **Request body parameters**:
+
+    *   ``name``
+    *   ``google_email``
+    *   ``google_as_members_authority``
+    """
+
     pass
 
 
 class GroupRemoveMemberView(GroupPermissionMixin, SingleObjectMixin, View):
+    """
+    Removes a person from the group.
+
+    Also removes the person from the Google group if the group has one specified.
+
+    **Success redirection view**: :class:`groups.views.GroupDetailView`
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+
+    **Path parameters**:
+
+    *   ``group_id`` - group ID
+    *   ``person_id`` - person ID
+    """
+
     model = Group
     pk_url_kwarg = "group_id"
-    success_message = "Osoba byla odebrána."
+    success_message = _("Osoba byla odebrána.")
 
     def get_success_url(self):
+        """:meta private:"""
+
         return reverse(
             "groups:detail", args=(self.kwargs[GroupRemoveMemberView.pk_url_kwarg],)
         )
 
     def get(self, request, *args, person_id, **kwargs):
+        """:meta private:"""
+
         group = self.get_object()
 
         member_to_remove_id = person_id
@@ -134,18 +247,38 @@ class GroupRemoveMemberView(GroupPermissionMixin, SingleObjectMixin, View):
 
 
 class SyncGroupMembersWithGoogleView(GroupPermissionMixin, SingleObjectMixin, View):
+    """
+    Synchronizes the group with its Google group, if it has one specified.
+
+    See :func:`groups.utils.sync_single_group_with_google` for more info.
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+
+    **Path parameters**:
+
+    *   ``group_id`` - group ID
+    """
+
     http_method_names = ["get"]
     model = Group
     pk_url_kwarg = "group_id"
+    success_message = _(
+        "Synchronizace skupiny {group_name} s Google Workplace byla úspěšná."
+    )
 
     def get(self, request, *args, **kwargs):
+        """:meta private:"""
+
         group = self.get_object()
 
         if group.google_email is None:
             error_message(
                 request,
                 _(
-                    "Zvolená skupina nemá zadanou google e-mailovou adresu, a proto nemůže být sychronizována."
+                    "Zvolená skupina nemá zadanou google e-mailovou adresu,"
+                    "a proto nemůže být sychronizována."
                 ),
             )
 
@@ -155,18 +288,29 @@ class SyncGroupMembersWithGoogleView(GroupPermissionMixin, SingleObjectMixin, Vi
 
         success_message(
             request,
-            _("Synchronizace skupiny {} s Google Workplace byla úspěšná.").format(
-                group.name
-            ),
+            self.success_message.format(group_name=group.name),
         )
 
         return redirect(reverse("groups:detail", args=[group.pk]))
 
 
 class SyncGroupMembersWithGoogleAllView(GroupPermissionMixin, View):
+    """
+    Synchronizes all groups with their Google groups.
+
+    See :class:`SyncGroupMembersWithGoogleView`
+    and  :func:`groups.utils.sync_single_group_with_google` for more info.
+
+    **Permissions**:
+
+    Users with ``skupiny`` permission.
+    """
+
     http_method_names = ["get"]
 
     def get(self, request, *args, **kwargs):
+        """:meta private:"""
+
         for group in Group.objects.filter(google_email__isnull=False):
             sync_single_group_with_google(group)
 
@@ -179,9 +323,11 @@ class SyncGroupMembersWithGoogleAllView(GroupPermissionMixin, View):
 
 
 class AddRemovePersonToGroupMixin(PersonPermissionMixin, MessagesMixin, UpdateView):
+    """:meta private:"""
+
     error_message: str
-    http_method_names = ["post"]
     form_class: type
+    http_method_names = ["post"]
     model = Person
 
     def get_success_url(self) -> str:
@@ -192,12 +338,48 @@ class AddRemovePersonToGroupMixin(PersonPermissionMixin, MessagesMixin, UpdateVi
 
 
 class AddPersonToGroupView(AddRemovePersonToGroupMixin):
+    """
+    Adds a person to a group.
+
+    **Success redirection view**: :class:`persons.views.PersonDetailView`
+
+    **Permissions**:
+
+    Users that manage the person's membership type.
+
+    **Path parameters**:
+
+    *   ``pk`` - person ID
+
+    **Request body parameters**:
+
+    *   ``group`` - group ID
+    """
+
+    error_message = _("Nepodařilo se přidat osobu do skupiny. ")
     form_class = AddPersonToGroupForm
     success_message = _("Osoba byla úspěšně přidána do skupiny.")
-    error_message = _("Nepodařilo se přidat osobu do skupiny. ")
 
 
 class RemovePersonFromGroupView(AddRemovePersonToGroupMixin):
+    """
+    Removes a person from a group.
+
+    **Success redirection view**: :class:`persons.views.PersonDetailView`
+
+    **Permissions**:
+
+    Users that manage the person's membership type.
+
+    **Path parameters**:
+
+    *   ``pk`` - person ID
+
+    **Request body parameters**:
+
+    *   ``group`` - group ID
+    """
+
+    error_message = _("Nepodařilo se odebrat osobu ze skupiny. ")
     form_class = RemovePersonFromGroupForm
     success_message = _("Osoba byla úspěšně odebrána ze skupiny.")
-    error_message = _("Nepodařilo se odebrat osobu ze skupiny. ")
